053e84f6a6929ef524fa2123d2c64a515c6c18b9
Saman <samangithoseinpour@gmail.com>
2023-11-11 16:12:05 +0330

    show statistics

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,5 +1,22 @@
-const Stats = () => {
-  return <div>Stats</div>;
-};
+import { stats } from '../constants.index';
+import styles from '../style';
+
+const Stats = () => (
+  <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
+    {stats.map((stat) => (
+      <div
+        className={`text-white flex-1 flex justify-center items-center flex-row m-3`}
+        key={stat.id}
+      >
+        <h4 className="font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-white">
+          {stat.value}
+        </h4>
+        <p className="font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px] text-gradient uppercase ml-3">
+          {stat.title}
+        </p>
+      </div>
+    ))}
+  </section>
+);
 
 export default Stats;
